8e81df8c06f24a4b2e2fce762a45e2f0e78be66e
GiAnG <yelliver@gmail.com>
2022-06-26 10:10:50 +0000

    Restore 0.0.1-dev from pub.dev

---
+++ lib/mini_sm.dart
@@ -0,0 +1,80 @@
+library mini_sm;
+
+import 'package:flutter/widgets.dart';
+
+Set<Value>? _context;
+
+class Value<T> {
+  Value(this._value);
+
+  T _value;
+
+  final Set<State> _states = {};
+
+  T get value {
+    _context?.add(this);
+    return _value;
+  }
+
+  set value(T value) {
+    _value = value;
+    update();
+  }
+
+  void update() => _states.forEach(_setState);
+
+  void _setState(State state) => state.setState(() {});
+
+  @override
+  String toString() => value.toString();
+}
+
+abstract class EWidget extends StatefulWidget {
+  const EWidget({super.key});
+
+  @override
+  StatefulElement createElement() => _EStatefulElement(this);
+}
+
+class _EStatefulElement extends StatefulElement {
+  _EStatefulElement(StatefulWidget widget) : super(widget);
+
+  final Set<Value> values = {};
+
+  @override
+  Widget build() {
+    try {
+      _context = values;
+      _context?.forEach(unregister);
+      _context?.clear();
+      return super.build();
+    } finally {
+      _context?.forEach(register);
+      _context = null;
+    }
+  }
+
+  @override
+  void unmount() {
+    values.forEach(unregister);
+    super.unmount();
+  }
+
+  void register(Value value) => value._states.add(state);
+
+  void unregister(Value value) => value._states.remove(state);
+}
+
+class IWidget extends EWidget {
+  const IWidget(this.builder, {Key? key}) : super(key: key);
+
+  final WidgetBuilder builder;
+
+  @override
+  State<IWidget> createState() => _IWidgetState();
+}
+
+class _IWidgetState extends State<IWidget> {
+  @override
+  Widget build(BuildContext context) => widget.builder(context);
+}
